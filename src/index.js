11e1db1e0c44085706a2661a68f29997ecd86cd2
viola-delia <viola.delia28@gmail.com>
2023-12-16 17:20:23 +1000

    Edited Javascript to inject more weather data through the API

--- src/index.js
+++ src/index.js
@@ -3,8 +3,36 @@ function refreshWeather(response) {
   let temperature = response.data.temperature.current;
   temperatureElement.innerHTML = Math.round(temperature);
 
+response.data.condition.description
+let descriptionElement = document.querySelector("#description");
+descriptionElement.innerHTML = response.data.condition.description;
+
+let humidityElement = document.querySelector("#humidity");
+humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+
+let windSpeedElement = document.querySelector("#wind-speed");
+windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`
+
+let timeElement = document.querySelector("#time");
+let date = new Date(response.data.time * 1000);
+timeElement.innerHTML = formatDate(date);
+
+
+
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.city;
+
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+  if (minutes <10) {
+    minutes = `0${minutes}`;
+  }
 }
 
 function searchCity(city) {
